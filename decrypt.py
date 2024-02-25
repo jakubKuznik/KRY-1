b765aee2290be3385578cd5c4eb922a2f2942489
jakubKuznik <kuznja16@gmail.com>
2024-02-25 19:09:13 +0100

    fixing table

--- decrypt.py
+++ decrypt.py
@@ -1,6 +1,7 @@
 import sys
 import pandas as pd 
 import io
+from llist import dllist
 
 alphabet = [
             'A','B','C','Č','D','E','Ě','F','G','H',
@@ -27,6 +28,16 @@ def createCipherAlphabet(day):
         index = (i + day) % len(alphabet)
         cipherAlphabet[index] = alphabet[i]
 
+    index = day-1; 
+    for i in range(len(alphabet)-1, 0, -1):
+        if cipherAlphabet[i] != '':
+            cipherAlphabet[index] = cipherAlphabet[i]
+            cipherAlphabet[i] = ''
+            index = index - 1
+            if index == -1:
+                break
+
+
 # Translate the numbers into the string using cipher table 
 def translate(str):
     str = str.replace(" ", "")
@@ -35,6 +46,7 @@ def translate(str):
     for i in range(0, len(str) - 1, 2):
         two_chars = str[i:i+2]
         result += countChar(int(two_chars))
+        
     
     return result
 
@@ -43,8 +55,7 @@ def countChar(num):
     # cannot be created in the cipher table 
     if num > 49: 
         return ''
-    return cipherAlphabet[num]
-
+    return str(cipherAlphabet[num])
 
 def LoadCzechDictionary(file):
     with open(file, 'r', encoding='utf-8') as file:
@@ -63,6 +74,13 @@ def is_prefix_of_czech_word(prefix):
     # Check if any word in the Czech dictionary starts with the given prefix
     for word in czech_dictionary:
         if word.startswith(prefix.lower()):
+            return True
+    return False
+
+def is_substring_of_czech_word(substring):
+    # Check if any word in the Czech dictionary starts with the given prefix
+    for word in czech_dictionary:
+        if substring in word:
             return True
     return False
 
@@ -91,6 +109,15 @@ def letters_in_row(df, general):
             if all_found: 
                 return i
 
+def find_letter_column(df, c, row):
+    
+    r = df.iloc[row].astype(str)
+    indices = []
+    for index, cell in enumerate(r):
+        if str(cell) == str(c):
+            indices.append(index)
+    return indices
+
 def switch_columns(df, c1, c2):
     # Store values from the 2nd column
     temp_values = df.iloc[:, c1].copy()
@@ -105,7 +132,6 @@ def get_n_word_in_row(df, n, row):
     
     word = ""
 
-    # Split the row by "-"
     r = df.iloc[row]
     
     word_count = 0; 
@@ -120,9 +146,58 @@ def get_n_word_in_row(df, n, row):
             continue
         word += cell
 
+# return list of word from index to index 
+# RETURN SUBSTRING IF WORD IS NO FINISHED 
+def words_from_to(df, row, fr, to): 
+    
+    word = ""
+    word_list = []
+    char_count = 0 
 
+    r = df.iloc[row]
 
+    for cell in r: 
+        if char_count < fr:  # Skip characters until 'from' index
+            char_count += 1
+        elif char_count >= to:  # Stop when 'to' index is reached
+            word_list.append(word)
+            return word_list
+        else:
+            if cell == '-':
+                word_list.append(word)
+                word = ""
+            if cell != '-':
+                word += cell
+        char_count += 1
+    word_list.append(word)
+    return word_list
 
+# in DF from a to b check if all word in all rows are prefixes 
+def check_if_czech_prefix(df, fr, to):
+    
+    index = 0
+    for r in df.iloc:
+        word_lists = words_from_to(df, index, fr, to)
+
+        for w in word_lists: 
+            if is_prefix_of_czech_word(w) == False: 
+                return False
+        index += 1
+    return True
+
+# in DF from a to b check if all word in all rows are substring  
+def check_if_czech_substring(df, fr, to):
+
+    print("check subs " + str(fr) +  " " + str(to)) 
+    index = 0
+    for r in df.iloc:
+        word_lists = words_from_to(df, index, fr, to)
+
+        for w in word_lists: 
+            if is_substring_of_czech_word(w) == False: 
+                return False
+        index += 1
+    return True
 
 def main():
     
@@ -160,9 +235,15 @@ def main():
 
     # Create cipher table 
     createCipherAlphabet(day) 
-    
+
     for i in encrypted_text:
         decrypted_text.append(translate(i))
+#        if '0' in translate(i):
+#            print("")
+#        if '4' in translate(i):
+#            print("CTYRKA")
+#        if 'O' in translate(i):
+#            print("Heureka ")
 
     csv = build_csv(decrypted_text)
 
@@ -174,32 +255,40 @@ def main():
 
     czech_dictionary = LoadCzechDictionary("Czech.dic")
     czech_dictionary.add(str(general))
-
     ### NOW BRUTE FORCE CHANGE THE COLLUMNS BUT LET EVERYTHING TO BE A CZECH WORD 
 
     ## first find the general 
 
-    special_letters = []
-    
+    #general = "xkuznio4"
+    print(general)
     row_general = letters_in_row(df, general)
-    print(row_general)
 
-    # print(special_letters)
-    # # First column 
-    # print(df[0])
-    # # first row 
-    # print(df.iloc[0]) 
-    # for cell in df.iloc[0]:
-        # print(cell)
+    first_column = 0
+    last_column = -1
+    bad = True
 
-    switch_columns(df, 0,1)
-    print(df)    
+
+    new_df = df.iloc[:, [1, 5]]
+    print(new_df)
+
+    dll = dllist()
+
+    for c in range(0, len(general)):
+        dll.append(find_letter_column(df, str(general[c]), row_general))
+
+    for item in dll:
+        print(item)
+
+#    switch_columns(df, 0,1)
+#    print(df)    
     
-    print("here")
-    # Example usage:
-    nth_word = get_n_word_in_row(df, 1, 1)  # Get the 3rd word from the first row
-    print(nth_word)
+#    print("here")
+#    # Example usage:
+#    nth_word = get_n_word_in_row(df, 1, 1)  # Get the 3rd word from the first row
+#    print(nth_word)
 
+#    print(check_if_czech_prefix(df,0, 0))
+#    print(words_from_to(df, 0, 0, 10))
 
 if __name__ == "__main__":
     main()
